e06efaad011ea127520633daa524c3aa12bc8582
Matias Grioni <matgrioni@gmail.com>
2017-09-13 19:28:24 -0400

    Added indentation and changed the relative paths to more absolute paths.

--- Preprocessing/mainFinishPPwithWWW.sh
+++ Preprocessing/mainFinishPPwithWWW.sh
@@ -2,31 +2,31 @@
 
 for d in */
 do
-echo $d
-for f in $d*.txt
-do
-echo $f
-cat $f | python ../removeIchars.py > $f.temp
-mv $f.temp $f
-cd /usr/local/words
-python mainExtractPossiblePOSTagsEXP2.py ../../../home/erdmann/NER/Preprocessing/Preprocessed/$f > ../../../home/erdmann/NER/Preprocessing/Preprocessed/$f.WWW 
-cd ../../../home/erdmann/NER/Preprocessing/Preprocessed/
+  echo $d
+  for f in $d*.txt
+  do
+    echo $f
+    cat $f | python ../removeIchars.py > $f.temp
+    mv $f.temp $f
+    cd /usr/local/words
+    python mainExtractPossiblePOSTagsEXP2.py ~/NER/Preprocessing/Preprocessed/$f > ~/NER/Preprocessing/Preprocessed/$f.WWW
+    cd ~/NER/Preprocessing/Preprocessed/
 
-echo William Whitakers Words is Done Analyzing $f
-echo Now to filter POS tags by Analysis
-echo
+    echo William Whitakers Words is Done Analyzing $f
+    echo Now to filter POS tags by Analysis
+    echo
 
-####################
-python ../filterPOSbyWWW2.py $f $f.WWW > $f.final
-cat $f.final | python ../removeColons4crf.py > $f
-rm $f.WWW
-rm $f.final
+    ####################
+    python ../filterPOSbyWWW2.py $f $f.WWW > $f.final
+    cat $f.final | python ../removeColons4crf.py > $f
+    rm $f.WWW
+    rm $f.final
 
-# python ../addMetaDataFeats.py $f $author $title > $f.txt
-# mv  $f.txt $f
+    # python ../addMetaDataFeats.py $f $author $title > $f.txt
+    # mv  $f.txt $f
 
-echo Done filtering POS tags by WWW analysis for $f
-echo
-####################
-done
+    echo Done filtering POS tags by WWW analysis for $f
+    echo
+    ####################
+  done
 done
