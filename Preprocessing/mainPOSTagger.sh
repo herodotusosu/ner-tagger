ccc68bd5f63e92820a70a197080679749c2865fb
Matias Grioni <matgrioni@gmail.com>
2017-09-26 10:39:31 -0400

    Preprocessor for RDRPOSTagger and add RDRPOSTagger to mainPOSTagger.sh

--- Preprocessing/mainPOSTagger.sh
+++ Preprocessing/mainPOSTagger.sh
@@ -1,13 +1,44 @@
+#!/usr/bin/env bash
+
+#
+# A caller of many different POS taggers. Pass in the location to search for
+# files to tag and pass in the taggers you would like to use. This last feature
+# is not made yet, and I'm not sure yet if I will add it. Right now it simply,
+# does all POS analyses (TreeTagger and RDRPOSTagger).
+#
+
+cur=$(pwd)
+
 for d in */
 do
   echo $d
   for f in $d*.txt
   do
-    echo $f
+    abs_text_loc="$cur/$f"
+
     ### DO i Need to replace !'s with .'s before sending to Tree Tagger?
-    cat $f | tree-tagger-latin > $f.pos
-    python ../preProcess.py $f.pos | python ../removeDoubles.py > $f
-    #rm $f.pos
+    # TreeTagger
+    #
+    # Use the Latin TreeTagger than split on sentences and remove double blank
+    # lines.
+    tree-tagger-latin < $f > $f.tmp
+    python ../preProcessTree.py $f.tmp | python ../removeDoubles.py > $f.tt
+    rm $f.tmp
+
+    # RDRPOSTagger
+    #
+    # Head to the RDRPOSTagger and use the pretrained latin models on UD to tag
+    # the current corpus file.
+    cd ~/RDRPOSTagger/jSCRDRtagger
+
+    latin_trained_loc='../Models/UniPOS/UD_Latin'
+    latin_model="${latin_trained_loc}/la-upos.RDR"
+    latin_dict="${latin_trained_loc}/la-upos.DICT"
+    java RDRPOSTagger $latin_model $latin_dict $abs_text_loc
+    cd $cur
+
+    python ../preProcessRDR.py $f.TAGGED | python ../removeDoubles.py > $f.rdr
+    rm $f.TAGGED
   done
 done
 
---
+++ Preprocessing/preProcessRDR.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+#
+# Preprocess output from the RDRPOSTagger specifically. The name of the POS
+# tagged file is passed in as the first command line argument. Preprocessing
+# consists of removing the LEAVEBLANK lines, and also having one word per line.
+# The output of the RDRPOSTagger has multiple words per line, it is a matter of
+# putting them on separate lines.
+#
+# Usage of this script is as follows:
+#   ./preProcessRDR.py filename.rdr > processed.rdr
+#
+
+import argparse
+
+
+BLANK_MARKER = 'LEAVEBLANK'
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('filename', help='The POS tagged file to preprocess.')
+args = parser.parse_args()
+
+with open(args.filename, 'r') as f:
+    for line in f:
+        line = line.strip()
+        tags = line.split()
+
+        for tag in tags:
+            split_idx = tag.rfind('/')
+            word = tag[:split_idx]
+            pos = tag[split_idx + 1:]
+
+            if word == BLANK_MARKER:
+                print
+            else:
+                print('{}\t{}'.format(word, pos))
+
+        if len(tags) == 0:
+            print
