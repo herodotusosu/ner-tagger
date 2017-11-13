39575f90181ff7bf06a250e4fedfbccd86ba9a55
Matias Grioni <matgrioni@gmail.com>
2017-11-13 17:55:25 -0500

    Remove references to leaveblank, mwls, and token mapping.

--- preprocess/processRDR.py
+++ preprocess/processRDR.py
@@ -6,24 +6,11 @@
 # consists of removing the LEAVEBLANK lines, and also having one word per line.
 # The output of the RDRPOSTagger has multiple words per line, it is a matter of
 # putting them on separate lines.
-#
 # Usage of this script is as follows:
 #   ./preProcessRDR.py filename.rdr > processed.rdr
 #
 
 import argparse
-
-
-BLANK_MARKER = 'LEAVEBLANK'
-
-
-def single_to_double_quote(token):
-    return '"'
-
-
-token_mappings = {
-    "'": single_to_double_quote
-}
 
 
 parser = argparse.ArgumentParser()
@@ -34,7 +21,6 @@ args = parser.parse_args()
 with open(args.filename, 'r') as f:
     for line in f:
         line = line.strip()
-        line = line.replace('!', '.')
         tags = line.split()
 
         for tag in tags:
@@ -42,16 +28,6 @@ with open(args.filename, 'r') as f:
             word = tag[:split_idx]
             pos = tag[split_idx + 1:]
 
-            try:
-                mapping = token_mappings[word]
-                word = mapping(word)
-            except KeyError:
-                pass
-
-            if word == BLANK_MARKER:
-                print
-            else:
-                print('{}\t{}'.format(word, pos))
-
+            print('{}\t{}'.format(word, pos))
         if len(tags) == 0:
             print
--- preprocess/processTT.py
+++ preprocess/processTT.py
@@ -15,11 +15,7 @@
 import argparse
 
 
-BLANK_MARKER = 'LEAVEBLANK'
-
 IGNORE_TOKENS = set(['«', '»', 'Â'])
-
-SPACE = ' '
 COL_DELIMETER = '\t'
 
 
@@ -29,25 +25,12 @@ args = parser.parse_args()
 
 
 with open(args.filename, 'r') as f:
-    for i, line in enumerate(f):
+    for line in f:
         line = line.strip()
         if line:
-            line = line.replace('!', '.')
-            cols = line.split(COL_DELIMETER)
-
-            # Change any blank intentions with actual blank lines.
-            # TODO: WHY THE FUCK IS THERE A LEAVEMOTHERFUCKINGBLANK?
-            if cols[0] == BLANK_MARKER:
-                print
             # Ignore all characters that are not appearing in the RDR output.
-            elif cols[0] not in IGNORE_TOKENS:
-                if SPACE in cols[0]:
-                    sub_words = cols[0].split(SPACE)
-                    for sub_word in sub_words:
-                        new_line_cols = [sub_word]
-                        new_line_cols.extend(cols[1:])
-                        new_line = COL_DELIMETER.join(new_line_cols)
-
-                        print(new_line)
-                else:
-                    print(line)
+            cols = line.split(COL_DELIMETER)
+            if cols[0] not in IGNORE_TOKENS:
+                print(line)
+        else:
+            print
