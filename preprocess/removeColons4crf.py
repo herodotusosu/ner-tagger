26e10340228df4d2ade497c2e6a60fef47534669
Matias Grioni <matgrioni@gmail.com>
2017-11-13 17:54:32 -0500

    Deleted utterly pointless files from a time when a worse programmer was on this project.

--- preprocess/removeColons4crf.py
+++
@@ -1,9 +0,0 @@
-### run with "cat name_of_file.html | python ../perseusExtractor.py"
-import sys
-import string
-
-output = {}
-
-for line in sys.stdin:
-	line = line.replace(':','<COLON>')
-	print line[0:-1]
