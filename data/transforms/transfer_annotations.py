b75bddf0fa4525d23f7b889dcc5a815dd228960e
Matias Grioni <matgrioni@gmail.com>
2017-11-16 17:30:56 -0500

    Created script to transfer annotations.

---
+++ data/transforms/transfer_annotations.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+#
+# Transfer annotations from one file to another. These files are in the format
+# of the combined analyses from the morphological analyzer and pos tagger. This
+# means each line is a token with a list of relevant features and a NER
+# annotation. This script facilitates transferring these annotations between
+# different analyses, such as when swapping out the pos tagger. Note that the
+# two files must be exactly aligned, token for token.
+#
+# Usage:
+#   ./transfer_annotations.py first.txt second.txt > output.txt
+#
+
+import argparse
+
+
+COL_DELIMITER = '\t'
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('source', file='The file with the original annotations.')
+parser.add_argument('dest', file='The file to transfer the annotations to.')
+args = parser.parse_args()
+
+
+with open(args.source, 'r') as source, open(args.dest, 'r') as dest:
+    for source_line, dest_line in zip(source, dest):
+        source_line = source_line.strip()
+        dest_line = dest_line.strip()
+
+        if source_line and dest_line:
+            source_cols = source_line.split(COL_DELIMITER)
+            dest_cols = dest_line.split(COL_DELIMITER)
+
+            # Transfer the analysis. Note this assumes the two files are
+            # aligned!
+            dest_cols[0] = source_cols[0]
+            new_dest_line = COL_DELIMITER.join(dest_cols)
+        else:
+            new_dest_line = dest_line
+
+        print(new_dest_line)
